fb39c95f611e50179a6db9c29b05a738e95f203e
Tim Simms <tsimms@gmail.com>
2022-12-01 12:45:59 -0600

    try a 'better' version

--- 0x09-slide_line/0-slide_line.c
+++ 0x09-slide_line/0-slide_line.c
@@ -11,63 +11,28 @@ int slide_line(int *line, size_t size, int direction)
 {
 	if (!line || size < 1)
 		return (0);
-	if (direction == SLIDE_LEFT)
-		return (slide_left(line, size));
-	if (direction == SLIDE_RIGHT)
-		return (slide_right(line, size));
+	if (direction == SLIDE_LEFT || direction == SLIDE_RIGHT)
+		return (slide_it(line, size, direction));
 	return (0);
 }
 
-/**
-* add_left - adds the left side of the array
-* @line: array
-* @size: size of the array
-*/
-void add_left(int *line, size_t size)
-{
-	size_t k;
 
-	for (k = 0; k < size - 1; k++)
-	{
-		if (line[k] == line[k + 1])
-		{
-			line[k] = line[k] + line[k + 1];
-			line[k + 1] = 0;
-		}
-	}
-}
-
-/**
-* add_right - adds the left side of the array
-* @line: array
-* @size: size of the array
-*/
-void add_right(int *line, size_t size)
-{
-	size_t k;
-
-	for (k = size - 1; k > 0; k--)
-	{
-		if (line[k] == line[k - 1])
-		{
-			line[k] = line[k] + line[k - 1];
-			line[k - 1] = 0;
-		}
-	}
-
-}
 
 
 /**
-* slide_left - slides and merges an array of integers to the
+* slide_it - slides and merges an array of integers to the
 * left if intergers match
 * @line: pointer to array
 * @size: size of array
+* @direction: direction to merge
 * Return: 1 on success, 0 on failure
 */
-int slide_left(int *line, size_t size)
+int slide_it(int *line, size_t size, int direction)
 {
 	size_t i, j;
+
+	if (direction == SLIDE_RIGHT)
+		reverse_array(line, size);
 
 	for (i = 0; i < size; i++)
 	{
@@ -84,8 +49,7 @@ int slide_left(int *line, size_t size)
 			}
 		}
 	}
-	/* merge ints if match */
-	add_left(line, size);
+	add_array(line, size);
 	/* slide greater than 0 ints to left most index */
 	for (i = 0; i < size; i++)
 	{
@@ -102,49 +66,43 @@ int slide_left(int *line, size_t size)
 			}
 		}
 	}
+	if (direction == SLIDE_RIGHT)
+		reverse_array(line, size);
 	return (1);
 }
 
 /**
-* slide_right - slides and merges an array of integers to the right if
-* intergers match and move all zeros to the left
-* @line: pointer to array
-* @size: size of array
-* Return: 1 if success, 0 if fail
+* reverse_array - reverses an array
+* @line: array
+* @size: size of the array
 */
-int slide_right(int *line, size_t size)
+void reverse_array(int *line, size_t size)
 {
-	size_t i, j;
+	int i, j, temp;
 
-	/* group ints together */
-	for (i = size - 1; i <= size; i--)
+	for (i = 0, j = size - 1; i < j; i++, j--)
 	{
-		if (line[i] == 0)
+		temp = line[i];
+		line[i] = line[j];
+		line[j] = temp;
+	}
+}
+
+/**
+* add_array - adds the left side of the array
+* @line: array
+* @size: size of the array
+*/
+void add_array(int *line, size_t size)
+{
+	size_t k;
+
+	for (k = 0; k < size - 1; k++)
+	{
+		if (line[k] == line[k + 1])
 		{
-			for (j = i; j <= size; j--)
-			{
-				if (line[j] != 0)
-				{
-					line[i] = line[j];
-					line[j] = 0;
-					break;
-				}
-			}
+			line[k] = line[k] + line[k + 1];
+			line[k + 1] = 0;
 		}
 	}
-	/* merge ints if match */
-	add_right(line, size);
-	/* slide greater than 0 ints to right most index */
-	for (i = 0; i < size - 1; i++)
-	{
-		for (j = 0; j < size - 1; j++)
-		{
-			if (line[j] > 0 && line[j + 1] == 0)
-			{
-				line[j + 1] = line[j];
-				line[j] = 0;
-			}
-		}
-	}
-	return (1);
 }
