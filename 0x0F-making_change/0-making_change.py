aa7ccae66b4bf7298a0401a33943446ea15803c8
Tim Simms <tsimms@gmail.com>
2023-02-25 10:25:23 -0600

    move return conditions

--- 0x0F-making_change/0-making_change.py
+++ 0x0F-making_change/0-making_change.py
@@ -5,12 +5,23 @@
 
 
 def makeChange(coins, total):
-    if total < 1:
-        return 0
-    totalCoins = 0
+    """Determine the fewest number of coins needed to make
+    change for a given amount of money.
+    Args:
+        coins (list): A list of coin values.
+        total (int): The total amount of money to make change for.
+    Returns:
+        The fewest number of coins needed to make change for the
+        given amount of money. If change cannot be made, return -1.
+    """
     coins.sort(reverse=True)
+    totalCoins = 0
     for coin in coins:
         if total >= coin:
             totalCoins += total // coin
             total %= coin
-    return totalCoins if totalCoins else -1
+    if total == 0:
+        return totalCoins
+    if total < 1:
+        return 0
+    return -1
