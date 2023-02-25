330aa363f4627561e4aad4eef90324bcef215acb
Tim Simms <tsimms@gmail.com>
2023-02-25 10:15:16 -0600

    removed second loop by usig divison and %

--- 0x0F-making_change/0-making_change.py
+++ 0x0F-making_change/0-making_change.py
@@ -5,6 +5,15 @@
 
 
 def makeChange(coins, total):
+    """Determine the fewest number of coins needed to make
+    change for a given amount of money.
+    Args:
+        coins (list): A list of coin values.
+        total (int): The total amount of money to make change for.
+    Returns:
+        The fewest number of coins needed to make change for the
+        given amount of money. If change cannot be made, return -1.
+    """
     if total < 1:
         return 0
     totalCoins = 0
