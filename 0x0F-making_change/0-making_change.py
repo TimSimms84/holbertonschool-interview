32a2c5ffa8688f423c8306168100f6d15f4283aa
Tim Simms <tsimms@gmail.com>
2023-02-25 10:20:57 -0600

    test

--- 0x0F-making_change/0-making_change.py
+++ 0x0F-making_change/0-making_change.py
@@ -5,15 +5,6 @@
 
 
 def makeChange(coins, total):
-    """Determine the fewest number of coins needed to make
-    change for a given amount of money.
-    Args:
-        coins (list): A list of coin values.
-        total (int): The total amount of money to make change for.
-    Returns:
-        The fewest number of coins needed to make change for the
-        given amount of money. If change cannot be made, return -1.
-    """
     if total < 1:
         return 0
     totalCoins = 0
@@ -22,4 +13,4 @@ def makeChange(coins, total):
         if total >= coin:
             totalCoins += total // coin
             total %= coin
-    return totalCoins if total == 0 else -1
+    return totalCoins if totalCoins else -1
