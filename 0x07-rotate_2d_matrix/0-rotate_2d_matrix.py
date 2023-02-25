e64fdc7ee6c5e6000107e1c1b09e409fd17d9c40
Tim Simms <tsimms@gmail.com>
2023-02-25 09:07:33 -0600

    changed '/env python3 to /python3'

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -9,13 +9,9 @@ def rotate_2d_matrix(matrix):
         matrix (list): 2D matrix
     """
 
-    # get length of matrix
     length = len(matrix)
-    # create new matrix with same length
     result = [[0] * length for _ in range(length)]
-    # iterate through matrix and assign values to new matrix
     for i in range(length):
         for j in range(length):
             result[j][length-1-i] = matrix[i][j]
-    # reassign matrix to new matrix
     matrix[:] = result[:]
